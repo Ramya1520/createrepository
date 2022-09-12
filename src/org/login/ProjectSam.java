149328e472146596137b5e2d7952b552f6d977f6
Ramya1520 <ramya.krishnan1520@gamil.com>
2022-09-13 01:28:01 +0530

    message for the commit

---
+++ src/org/login/ProjectSam.java
@@ -0,0 +1,15 @@
+package org.login;
+
+public class ProjectSam {
+public static void main(String[]args) {
+		
+		int a[]= {1,2,3,4,5,6,7,8,9,10};
+		int j=0;
+		
+		for(int i=0; i<=10; i++) {
+			j=j+i;
+		}
+		System.out.println("sum: "+ j);
+	}
+
+}
